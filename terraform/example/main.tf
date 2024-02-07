2f0c074e75a84be16099da12254fce27ccb4209a
mpeters0n <2matthew.peterson@gmail.com>
2024-02-06 16:15:34 -0800

    new attempt

--- terraform/example/main.tf
+++ terraform/example/main.tf
@@ -1,74 +1,10 @@
-locals {
-  app_name = "example"
-  app_team = "devops"
-  env      = "dev"
+variable "subject" {
+  type = string
+  default = "World"
+  description = "Subject to hello"
 }
 
-provider "kubernetes" {
-  config_path    = "/home/mpeterson/kind.kubeconfig"
-  config_context = "kind"
+output "hello_world" {
+  value = "Hello, ${var.subject}!"
 }
 
-#resource "kubernetes_namespace" "this" {
-#  metadata {
-#    name = "example"
-#  }
-#}
-
-resource "kubernetes_deployment" "this" {
-  metadata {
-    name      = local.app_name
-#    namespace = kubernetes_namespace.this.metadata.0.name
-    namespace = "flux-system"
-  }
-
-  spec {
-    replicas = 1
-
-    selector {
-      match_labels = {
-        "app.kubernetes.io/name" = local.app_name
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          "app.kubernetes.io/name" = local.app_name
-        }
-      }
-      spec {
-        container {
-          image = "nginx:1.21.6"
-          name  = "example"
-
-          resources {
-            limits = {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests = {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
-          }
-
-          liveness_probe {
-            http_get {
-              path = "/"
-              port = 80
-
-              http_header {
-                name  = "X-Custom-Header"
-                value = "Awesome"
-              }
-            }
-
-            initial_delay_seconds = 3
-            period_seconds        = 3
-          }
-        }
-      }
-    }
-  }
-}
